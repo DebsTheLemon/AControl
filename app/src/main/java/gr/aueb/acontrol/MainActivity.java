2eb150a608cca8c26e20069229a5d3c211523cd8
DebsTheLemon <depi.ge@hotmail.com>
2022-05-09 18:01:56 +0300

    Splash Screen that just wont work

--- app/src/main/java/gr/aueb/acontrol/MainActivity.java
+++ app/src/main/java/gr/aueb/acontrol/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_AControl);
         setContentView(R.layout.activity_main);
 
         mIvToggle = (ImageView) findViewById(R.id.PowerBtn);
