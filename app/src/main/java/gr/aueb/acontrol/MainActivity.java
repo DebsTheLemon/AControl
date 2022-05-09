7bd97d55022337b65420b81461c949efb010307c
DebsTheLemon <depi.ge@hotmail.com>
2022-05-09 16:10:59 +0300

    Image Radio Buttons for Mode Selection Menu / Icon / Dark Theme / Arrow Buttons pressed feature

--- app/src/main/java/gr/aueb/acontrol/MainActivity.java
+++ app/src/main/java/gr/aueb/acontrol/MainActivity.java
@@ -14,22 +14,23 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-import gr.aueb.acontrol.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 import android.widget.ImageView;
 
+
 public class MainActivity extends Activity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
     int temp = 24;
-
+    CompoundButton previousCheckedCompoundButton;
     ImageView mIvToggle;
 
     @Override
@@ -45,7 +46,33 @@ public class MainActivity extends Activity {
                 mIvToggle.setActivated(!mIvToggle.isActivated());
             }
         });
+
+        RadioButton cool, fan, dry, heat, auto;
+        cool = (RadioButton) findViewById(R.id.Cool);
+        fan = (RadioButton) findViewById(R.id.Fan);
+        dry = (RadioButton) findViewById(R.id.Dry);
+        heat = (RadioButton) findViewById(R.id.Heat);
+        auto = (RadioButton) findViewById(R.id.Auto);
+
+        cool.setOnCheckedChangeListener(onRadioButtonCheckedListener);
+        fan.setOnCheckedChangeListener(onRadioButtonCheckedListener);
+        dry.setOnCheckedChangeListener(onRadioButtonCheckedListener);
+        heat.setOnCheckedChangeListener(onRadioButtonCheckedListener);
+        auto.setOnCheckedChangeListener(onRadioButtonCheckedListener);
     }
+
+    CompoundButton.OnCheckedChangeListener onRadioButtonCheckedListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (!isChecked) return;
+            if (previousCheckedCompoundButton != null) {
+                previousCheckedCompoundButton.setChecked(false);
+                previousCheckedCompoundButton = buttonView;
+            } else {
+                previousCheckedCompoundButton = buttonView;
+            }
+        }
+    };
 
     public void increaseInteger(View view) {
         if (temp < 32) {
