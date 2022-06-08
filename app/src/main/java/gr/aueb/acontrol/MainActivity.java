4d1c59fd03e10f8e5ff180c0977906077730d0f4
DebsTheLemon <depi.ge@hotmail.com>
2022-06-08 17:01:18 +0300

    Voice Commands & UI Tweaking

--- app/src/main/java/gr/aueb/acontrol/MainActivity.java
+++ app/src/main/java/gr/aueb/acontrol/MainActivity.java
@@ -1,9 +1,11 @@
 package gr.aueb.acontrol;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import android.speech.RecognizerIntent;
@@ -20,13 +22,19 @@ import android.widget.Toast;
 import android.widget.ImageView;
 import android.speech.tts.TextToSpeech;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 
 public class MainActivity extends Activity {
 
     static int temp = 26;
+    private static int MICROPHONE_PERMISSION_CODE = 200;
     CompoundButton previousCheckedCompoundButton;
 
     private static MainActivity instance;
@@ -210,7 +218,7 @@ public class MainActivity extends Activity {
 
     private void displayTemp(int temp) {
         TextView displayInteger = (TextView) findViewById (R.id.CurrentTempNumber);
-        displayInteger.setText("" + temp);
+        displayInteger.setText(String.valueOf(temp));
     }
 
     private void displayMode(String mode) {
@@ -224,12 +232,12 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
-
-        if(intent.resolveActivity(getPackageManager()) != null) {
+        getMicPermission();
+//        if(intent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(intent, 10);
-        }else{
-            Toast.makeText(this, "Your device does not support speech input", Toast.LENGTH_SHORT).show();
-        }
+//        }else{
+//            Toast.makeText(this, "Your device does not support speech input", Toast.LENGTH_SHORT).show();
+//        }
     }
 
     @Override
@@ -238,12 +246,39 @@ public class MainActivity extends Activity {
 
         switch (requestCode){
             case 10:
-                if (resultCode == RESULT_OK && data != null){
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    Log.i("Speech recognition: I heard", String.valueOf(result));
+                if (resultCode == RESULT_OK && data != null) {
+                    String result = String.valueOf(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0));
+                    String results[] = result.trim().split("\\s* \\s*");
+                    Log.i("Speech recognition", "I heard you!");
+                    if (result.contains("assistance")){
+                        listCommands();
+                    }else if (result.contains("turn") && result.contains("on")){
+                        powerON();
+                    }else if (result.contains("turn") && result.contains("off")){
+                        powerOFF();
+                    }else if (result.contains("Celsius")){
+                        Log.i("Desired Temperature", String.valueOf(results[results.length-2]));
+                        temp = Integer.valueOf(results[results.length-2]);
+                        displayTemp(temp);
+                    }
                 }
-
                 break;
+        }
+    }
+
+    private void listCommands() {
+        String commands =   "Say 'TURN ON' to turn the AC ON."+
+                            "Say 'TURN OFF' to turn the AC OFF. "+
+                            "To set your desired temperature say the temperature value followed by the word Celsius."+
+                            "To change the mode say: 'mode', and the name of the mode you want, COOL, FAN, DRY, HEAT or AUTO";
+
+        audio.speak(commands, TextToSpeech.QUEUE_FLUSH, null);
+
+    }
+
+    private void getMicPermission () {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECORD_AUDIO}, MICROPHONE_PERMISSION_CODE);
         }
     }
 }
